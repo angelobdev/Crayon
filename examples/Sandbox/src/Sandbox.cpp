8fbc8940b555436387a3f02621bef269af95c34c
angelobdev <angelo.belcastro01@gmail.com>
2023-04-12 20:21:18 +0200

    Updated Sandbox

--- examples/Sandbox/src/Sandbox.cpp
+++ examples/Sandbox/src/Sandbox.cpp
@@ -30,24 +30,25 @@ public:
               sourceCode(Crayon::ResourceLoader::LoadFileAsString("assets/shaders/basic.glsl")),
               shader(sourceCode)
     {
-        glClearColor(0.2f, 0.4f, 0.6f, 1.0f);
+        GLCall(glClearColor(0.2f, 0.4f, 0.6f, 1.0f));
 
         // Vertex Array
-        glGenVertexArrays(1, &vao);
+        GLCall(glGenVertexArrays(1, &vao));
+        GLCall(glBindVertexArray(vao));
 
         // Vertex Buffer
-        glGenBuffers(1, &vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, vertices.size(), &vertices[0], GL_STATIC_DRAW);
+        GLCall(glGenBuffers(1, &vbo));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+        GLCall(glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW));
 
         // Link to VA
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (const void *) 0);
+        GLCall(glEnableVertexAttribArray(0));
+        GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (const void*)0));
 
         // Index Buffer
-        glGenBuffers(1, &ibo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), &indices[0], GL_STATIC_DRAW);
+        GLCall(glGenBuffers(1, &ibo));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+        GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW));
 
         // SHADER
         CRAYON_TRACE("Shader source code:\n{}", sourceCode);
@@ -68,11 +69,13 @@ public:
     void Render() override
     {
         // CUSTOM RENDERING
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
         shader.Bind();
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+
+        GLCall(glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr));
     }
 
     void RenderUI() override
